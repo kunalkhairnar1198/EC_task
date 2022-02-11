e0cd6aa9012bfbb8c913cfeaa78eb2caf9d79a74
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2022-02-11 15:07:39 +0530

    postgis_setup_and_migration_superuser

--- EC_task/settings.py
+++ EC_task/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'EC_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'weather_data',
+        'USER': 'weather_data',
+        'PASSWORD':'weather_data@1198',
+        'HOST':'localhost',
+        'PORT':5432
+
     }
 }
+
 
 
 # Password validation
